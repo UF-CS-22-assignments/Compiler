0065670ea85d94636c98cfaff7fccd95e2449860
NJUleo <1250925329@qq.com>
2022-09-04 16:06:34 -0400

    chore: some code format

--- src/main/java/edu/ufl/cise/plpfa22/ILexer.java
+++ src/main/java/edu/ufl/cise/plpfa22/ILexer.java
@@ -6,16 +6,20 @@ package edu.ufl.cise.plpfa22;
 public interface ILexer {
 
 	/**
-	 * Return an IToken and advance the internal position so that subsequent calls will return subsequent ITokens.
+	 * Return an IToken and advance the internal position so that subsequent calls
+	 * will return subsequent ITokens.
+	 * 
 	 * @return
 	 * @throws LexicalException
 	 */
 	IToken next() throws LexicalException;
-	
+
 	/**
-	 * Return an IToken without advancing the internal position.  A subsequent call to next or peek will return the same IToken. 
+	 * Return an IToken without advancing the internal position. A subsequent call
+	 * to next or peek will return the same IToken.
+	 * 
 	 * @return
 	 * @throws LexicalException
 	 */
-	IToken peek() throws LexicalException;  
+	IToken peek() throws LexicalException;
 }
