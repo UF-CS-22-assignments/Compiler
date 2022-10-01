cbd5447155a406e11c89fef59fef3bc534b0418f
NJUleo <1250925329@qq.com>
2022-09-30 22:47:06 -0400

    chore: remove unused import in SyntaxException

--- src/main/java/edu/ufl/cise/plpfa22/SyntaxException.java
+++ src/main/java/edu/ufl/cise/plpfa22/SyntaxException.java
@@ -1,7 +1,5 @@
 package edu.ufl.cise.plpfa22;
 
-import edu.ufl.cise.plpfa22.IToken.SourceLocation;
-
 @SuppressWarnings("serial")
 public class SyntaxException extends PLPException {
 
@@ -12,10 +10,6 @@ public class SyntaxException extends PLPException {
 	public SyntaxException(String error_message, int line, int column) {
 		super(error_message, line, column);
 	}
-
-	// public SyntaxException(String error_message, SourceLocation loc) {
-	// super(error_message, loc);
-	// }
 
 	public SyntaxException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
 		super(message, cause, enableSuppression, writableStackTrace);
