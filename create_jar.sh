246bafb620158b83ea4635bef1131609e52a3445
NJUleo <1250925329@qq.com>
2022-11-19 15:53:38 -0500

    feat(code gen): second half of the provided code for code generation

--- create_jar.sh
+++ create_jar.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-name="Zhang_Li_HW4"
+name="Zhang_Li_HW5"
 zip -r $name.zip . -x ./target/*
 cd src/main/java
 find . -name *.java > javaFiles
--- src/main/java/edu/ufl/cise/plpfa22/CodeGenUtils.java
+++ src/main/java/edu/ufl/cise/plpfa22/CodeGenUtils.java
@@ -11,6 +11,7 @@ package edu.ufl.cise.plpfa22;
  */
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.List;
 
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.MethodVisitor;
@@ -19,7 +20,41 @@ import org.objectweb.asm.util.TraceClassVisitor;
 
 import edu.ufl.cise.plpfa22.ast.Types.Type;
 
-public class CodeGenUtils {
+public class CodeGenUtils{
+	
+	public record GenClass(String className, byte[] byteCode) {}
+	
+	
+	/**
+	 * Converts a classNume in Java notation with . separator to JVM classname 
+	 * with / separator
+	 * 
+	 * If already in JVM form, the original String is returned
+	 * 
+	 * @param className
+	 * @return
+	 */
+	static String toJMVClassName(String className) {
+		return className.replace('.','/');
+	}
+	
+	
+	/**
+	 * Converts a classNume in JVM notation with / separator to Java style classname 
+	 * with . separator
+	 * 
+	 * If already in Java-style form, the original String is returned 
+	 * 
+	 * @param className
+	 * @return
+	 */
+	static String toJavaClassName(String jvmClassName) {
+		return jvmClassName.replace('/', '.');
+	}
+	
+	static String toJVMClassDesc(String className) {
+		return "L"+ toJMVClassName(className)+";";
+	}
 
 	/**
 	 * Converts the provided byte array
@@ -35,26 +70,39 @@ public class CodeGenUtils {
 		cr.accept(new TraceClassVisitor(new PrintWriter(out)), flags);
 		return out.toString();
 	}
-
+	
 	/**
-	 * Loader for dynamically generated classes.
-	 * Instantiated by getInstance.
-	 *
 	 */
 	public static class DynamicClassLoader extends ClassLoader {
+
 		public DynamicClassLoader(ClassLoader parent) {
 			super(parent);
+		}
+		
+		public DynamicClassLoader() {
+			super();
 		}
 
 		public Class<?> define(String className, byte[] bytecode) {
 			return super.defineClass(className, bytecode, 0, bytecode.length);
 		}
-	};
 
+		//requires mainclass to be first class in list
+		public Class<?> define(List<GenClass> generatedClasses) {
+			Class<?> mainClass = null;
+			for (GenClass genClass : generatedClasses) {
+				Class<?> cl = define(toJavaClassName(genClass.className()), genClass.byteCode());
+				if (mainClass == null)
+					mainClass = cl;
+			}
+			return mainClass;
+		}
+
+	};
+	
 	/**
 	 * Use for debugging only.
-	 * Generates code to print the given String followed by ; to the standard output
-	 * to allow observation of execution of generated program
+	 * Generates code to print the given String followed by ; to the standard output to allow observation of execution of generated program
 	 * during development.
 	 * 
 	 * @param mv
@@ -68,26 +116,28 @@ public class CodeGenUtils {
 
 	/**
 	 * Use for debugging only
-	 * Generates code to print the value on top of the stack to the standard output
-	 * without consuming it.
+	 * Generates code to print the value on top of the stack to the standard output without consuming it.
 	 * Requires stack not empty
-	 *
+     *
 	 * @param mv
 	 * @param type
 	 */
 	public static void genDebugPrintTOS(MethodVisitor mv, Type type) {
-		mv.visitInsn(Opcodes.DUP);
-		mv.visitFieldInsn(Opcodes.GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
-		mv.visitInsn(Opcodes.SWAP);
-		if (type.equals(Type.NUMBER)) {
-			mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/io/PrintStream", "print", "(I)V", false);
-		} else if (type.equals(Type.BOOLEAN)) {
-			mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/io/PrintStream", "print", "(Z)V", false);
-		} else if (type.equals(Type.STRING)) {
-			mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/io/PrintStream", "print", "(Ljava/lang/String;)V", false);
-		} else
+			mv.visitInsn(Opcodes.DUP);
 			mv.visitFieldInsn(Opcodes.GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
-		mv.visitLdcInsn(";\n");
-		mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/io/PrintStream", "print", "(Ljava/lang/String;)V", false);
+			mv.visitInsn(Opcodes.SWAP);
+			if (type.equals(Type.NUMBER)) {
+				mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/io/PrintStream", "print", "(I)V", false);
+			}	
+			else if (type.equals(Type.BOOLEAN)) {
+				mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/io/PrintStream", "print", "(Z)V", false);
+			}
+			else if (type.equals(Type.STRING)) {
+				mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/io/PrintStream", "print", "(Ljava/lang/String;)V", false);
+			}
+			else
+			mv.visitFieldInsn(Opcodes.GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
+			mv.visitLdcInsn(";\n");
+			mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/io/PrintStream", "print", "(Ljava/lang/String;)V", false);				
 	}
 }
